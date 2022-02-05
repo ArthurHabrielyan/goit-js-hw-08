1e4a934850677b9e84ba275f9eae95b7db82e1ea
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-02-05 16:35:58 +0200

    form-created

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,30 @@
+const throttle = require("lodash.throttle");
+
 const emailForm = document.querySelector(".feedback-form");
 const inputValue = document.querySelector('[name="email"]');
 const messageValue = document.querySelector('[name="message"]');
 const buttonForSubmit = document.querySelector('button[type="submit"]');
 const FEEDBACK_FROM_STATE = "feedback-form-state";
 
+console.log(JSON.parse(localStorage.getItem(FEEDBACK_FROM_STATE)));
+
+const emailValueFromStorage =
+  JSON.parse(localStorage.getItem(FEEDBACK_FROM_STATE)) &&
+  JSON.parse(localStorage.getItem(FEEDBACK_FROM_STATE)).email;
+
+const messageValueFromStorage =
+  JSON.parse(localStorage.getItem(FEEDBACK_FROM_STATE)) &&
+  JSON.parse(localStorage.getItem(FEEDBACK_FROM_STATE)).message;
+
+if (emailValueFromStorage) {
+  inputValue.value = emailValueFromStorage;
+}
+if (messageValueFromStorage) {
+  messageValue.value = messageValueFromStorage;
+}
+
 const onEmailForm = function (e) {
+  console.log(inputValue.value, messageValue.value);
   localStorage.setItem(
     FEEDBACK_FROM_STATE,
     JSON.stringify({
@@ -14,27 +34,13 @@ const onEmailForm = function (e) {
   );
 };
 
-const emailValueFromStorage = JSON.parse(
-  localStorage.getItem(FEEDBACK_FROM_STATE)
-).email;
-
-const messageValueFromStorage = JSON.parse(
-  localStorage.getItem(FEEDBACK_FROM_STATE)
-).message;
-
-if (emailValueFromStorage) {
-  inputValue.value = emailValueFromStorage;
-}
-if (messageValueFromStorage) {
-  messageValue.value = messageValueFromStorage;
-}
+throttle(onEmailForm, 500);
 
 function onSavingData(e) {
   e.preventDefault();
   console.log(`email: ${inputValue.value}, message: ${messageValue.value}`);
   emailForm.reset();
   localStorage.clear();
-  emailForm.removeEventListener("input", onEmailForm);
 }
 
 emailForm.addEventListener("submit", onSavingData);
